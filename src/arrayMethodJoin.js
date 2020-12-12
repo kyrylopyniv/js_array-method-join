88c4712497f8612fd46b30c1a39abc2f5ae450c6
Kyrylo Pyniv <pynivkm@gmail.com>
2020-12-12 23:23:23 +0200

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,36 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let endString = '';
+    let sep = separator;
+
+    if (this.length === 0) { // умова для пустого масива
+      return endString;
+    }
+
+    if (this.length === 1) { // умова для масива довж 1
+      endString += this[0];
+
+      return endString;
+    }
+
+    if (separator === undefined) {
+      sep = ',';
+    }
+
+    if (this.length > 0) { // умова для масива довж > 0
+      for (let el = 0; el < this.length - 1; el++) {
+        if (this[el] === null || this[el] === undefined) {
+          this[el] = '';
+        }
+        endString += this[el];
+        endString += sep;
+      }
+    }
+
+    endString += this[this.length - 1];// додаємо останній ел
+
+    return endString;
   };
 }
 
